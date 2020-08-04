1407523315efdb0c324708f5cd57e7d9887991b8
tamirazrab <tamirazrab@gmail.com>
2020-08-04 23:07:27 +0500

    Finishing touches - added decoding part

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+const { hash } = window.location; /* this what contains encoded text in has fragment */
+const message = atob(hash.replace('#', ''));
+
+const inputForm = document.querySelector('#input-form');
+const outputForm = document.querySelector('#output-form');
+const displayForm = document.querySelector('#display-form');
+
+if (message) {
+    /* if there is message then hiding the input to prevent adding # bug */
+    inputForm.classList.add('hide');
+
+    /* remove hide by default from display form where encoded message will be displayed */
+    displayForm.classList.remove('hide');
+
+    /* add the decoded message for display */
+    document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault(); /* to disable browser default behaviour of taking input and sending it to back-end server */
 
     /* hiding input form */
-    document.querySelector('#input-form').classList.add('hide');
+    inputForm.classList.add('hide');
 
     /* showing output form - which is hidden by default */
-    document.querySelector('#output-form').classList.remove('hide');
+    outputForm.classList.remove('hide');
 
     const input = document.querySelector('#message-input');
-    const encrypted = btoa(input);
+    const encrypted = btoa(input.value);
 
     const displayMessage = document.querySelector('#display-message');
     /* window.location contains locations (URL) of file, back ticks required otherwise it will display whole window.location object with un necessary information.  */
